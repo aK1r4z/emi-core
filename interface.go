9a9ad6a0ac6b363c256ad08189db8e66c44a1854
aK1r4z_Laptop <akiraz@yeah.net>
2026-02-20 21:56:24 +0800

    roughly defined interfaces

---
+++ interface.go
@@ -0,0 +1,53 @@
+package emi_core
+
+import (
+	"context"
+
+	"github.com/aK1r4z/emi-core/api"
+	milky "github.com/aK1r4z/emi-core/types"
+)
+
+type Logger interface {
+	Tracef(format string, args ...any)
+	Debugf(format string, args ...any)
+	Infof(format string, args ...any)
+	Warnf(format string, args ...any)
+	Errorf(format string, args ...any)
+	Fatalf(format string, args ...any)
+
+	Trace(args ...any)
+	Debug(args ...any)
+	Info(args ...any)
+	Warn(args ...any)
+	Error(args ...any)
+	Fatal(args ...any)
+}
+
+type APIClient interface {
+	SendPrivateMessage(ctx context.Context, request api.SendPrivateMessageRequest)
+	SendGroupMessage(ctx context.Context, request api.SendGroupMessageRequest)
+	// [TODO] 包装更多 API 方法
+}
+
+type Bot interface {
+	Open(context.Context) error
+	Close() error
+
+	Wait()
+
+	APIClient
+
+	Handle(EventHandler)
+
+	Logger() Logger
+}
+
+type EventSource interface {
+	Open() (chan milky.RawEvent, error)
+	Close() error
+}
+
+type EventHandler interface {
+	milky.EventDescriber
+	Handle(*Bot, milky.Event, milky.RawEvent)
+}
